97d390c46a3e275583056b7c937a76c6e9bf0b3e
Matheus Patricio <patriciomtt@outlook.com>
2024-10-29 13:08:31 -0300

    py for algoritm

--- ex1/program.py
+++ ex1/program.py
@@ -1,22 +1,16 @@
 """
-
-DESAFIO - Talento Cloud - introdução a programação com Python.
-
-Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é 
-supersticioso e optou por não ter um 13ro andar.
+Desafio - Talento Cloud - Introdução a Programação com python.
+Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel 
+é supersticioso e optou por não ter um 13ro andar.
 
 Escreva um código que imprima todos os números exceto o número 13.
 Escreva mais dois códigos que resolvam o mesmo problema, mas dessa vez usando os outros dois tipos
- de laços de repetição aprendidos.
+de laços de repetição aprendidos.
 
 Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
 
 """
 
-
-for andar in range(20, 0, -1):
+for andar in range(20,0, -1):
     if andar != 13:
         print(andar)
-
-
-#para cada andar em um range de 20, iniciando em 0 e subtraindo 1 no contandor print andar
